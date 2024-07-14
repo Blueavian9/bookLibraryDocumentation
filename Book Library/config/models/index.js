9191dd21eb6f00f726cdcb177da862b3067e50bd
Cesar Aguilar <Blueavian9@gmail.com>
2024-07-14 11:45:26 -0700

    updated User Model

--- Book Library/config/models/index.js
+++ Book Library/config/models/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const dbConfig = require("../config/db.config");
+const User = require("./user.model");
 
 const sequelize = new Sequelize({
   dialect: dbConfig.dialect,
---
+++ Book Library/config/models/user.model.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { sequelize } = require("./index");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
+
+module.exports = User;
