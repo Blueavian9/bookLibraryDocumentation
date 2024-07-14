2258e8077309009d83901665af5ccdc846e34ab5
Cesar Aguilar <Blueavian9@gmail.com>
2024-07-14 16:01:16 -0700

    updated CRUD ops4Books

---
+++ Book Library/routes/book.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bookController = require("../controllers/book.controller");
+const { verifyToken } = require("../middleware/auth.middleware");
+
+const router = express.Router();
+
+router.post("/", verifyToken, bookController.createBook);
+router.get("/", verifyToken, bookController.getBooks);
+router.get("/:id", verifyToken, bookController.getBooksById);
+router.put("/:id", verifyToken, bookController.updateBook);
+router.delete("/:id", verifyToken, bookController.deleteBook);
+
+module.exports = router;
