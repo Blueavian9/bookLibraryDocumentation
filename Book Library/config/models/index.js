909a1ce1ee1a225f9d627955e0aa81059a74bd97
Cesar Aguilar <Blueavian9@gmail.com>
2024-07-14 17:43:44 -0700

    updated server troubleshooting

--- Book Library/config/models/index.js
+++ Book Library/config/models/index.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 const dbConfig = require("../config/db.config");
 const User = require("./user.model");
 const Book = require("./book.model");
+
 const sequelize = new Sequelize({
   dialect: dbConfig.dialect,
   storage: dbConfig.storage,
