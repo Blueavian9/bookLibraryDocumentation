7d7a68944ffcee87f8c4c77babdc91d99b6bc5b7
Cesar Aguilar <Blueavian9@gmail.com>
2024-09-18 10:32:47 -0700

    modified app.js

--- app.js
+++ app.js
@@ -2,20 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./models");
 
+// Import routes
+const auth = require("./routes/auth.routes.js");
+const book = require("./routes/book.routes.js");
+
 const app = express();
-
-// Use the routes
-app.use("/auth", auth);
-app.use("/books", book);
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  if (err.name === "UnauthorizedError") {
-    return res.status(401).send({ message: "Invalid token" });
-  }
-  res.status(500).send("Something broke!");
-});
 
 // CORS configuration
 app.use(cors());
@@ -26,14 +17,14 @@ app.use(express.json());
 // Parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// Use the routes
+app.use("/auth", auth);
+app.use("/books", book);
+
 // Simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the bookstore application." });
 });
-
-// Register routes
-const auth = require("./routes/auth.routes.js");
-const book = require("./routes/book.routes.js");
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -45,7 +36,6 @@ app.use((err, req, res, next) => {
 });
 
 // Database synchronization
-
 if (process.env.NODE_ENV !== "test") {
   db.sequelize
     .sync()
